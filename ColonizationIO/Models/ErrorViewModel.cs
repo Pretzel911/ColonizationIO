9eef1515f1f11898c68ab7f7bdaa975ae60652a6
Alex Scriven <ascriven@keihin-na.com>
2020-04-21 17:51:36 -0400

    Add project files.

---
+++ ColonizationIO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ColonizationIO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
